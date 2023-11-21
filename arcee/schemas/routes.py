a551b0f5f2db699d170b59250ab1299ec11280d7
jacobsolawetz <jacobsolawetz@gmail.com>
2023-11-21 09:13:17 -0600

    unlock demo dalms in langchain

--- arcee/schemas/routes.py
+++ arcee/schemas/routes.py
@@ -4,7 +4,7 @@ from strenum import StrEnum
 class Route(StrEnum):
     contexts = "contexts"
     train_model = "models/train"
-    train_model_status = "models/status/{id_or_name}"
+    train_model_status = "models/status/{id_or_name}?allow_demo=True"
     train_retriever = "retrievers/train"
     train_retriever_status = "retrievers/status/{id_or_name}"
     identity = "whoami"
