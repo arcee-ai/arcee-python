c6d4abe3eebb3e345bc1179067ab07211c9485ba
jacobsolawetz <jacobsolawetz@gmail.com>
2024-05-14 10:37:16 -0500

    method export

--- arcee/__init__.py
+++ arcee/__init__.py
@@ -13,4 +13,4 @@ if not config.ARCEE_API_KEY:
         config.ARCEE_API_KEY = input("ARCEE_API_KEY not found in environment. Please input api key: ")
     os.environ["ARCEE_API_KEY"] = config.ARCEE_API_KEY
 
-__all__ = ["upload_docs", "DALM", "DALMFilter", "upload_corpus_folder", "upload_qa_pairs", "start_alignment", "start_pretraining", "start_retriever_training", "get_retriever_status", "start_deployment", "stop_deployment", "generate", "retrieve", "delete_corpus", "upload_alignment"]
+__all__ = ["upload_docs", "DALM", "DALMFilter", "upload_corpus_folder", "upload_qa_pairs", "start_alignment", "start_pretraining", "start_retriever_training", "get_retriever_status", "start_deployment", "stop_deployment", "generate", "retrieve", "delete_corpus", "upload_alignment", "start_merging"]
